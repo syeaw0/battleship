a37022d983a15c74efc92068a02fccacc9290e24
syeaw0 <syeaw0@csu.fullerton.edu>
2018-09-08 09:53:50 -0700

    Test Commit

---
+++ src/ship.py
@@ -0,0 +1,20 @@
+# A change by Sam
+"""init ships using shipBuilder"""
+class ShipBuilder:
+    def __init__(self, name, length):
+        self.name = name
+        self.length = length
+
+
+#init ships here
+ship1 = ShipBuilder("Destroyer", 5)
+
+
+#place ships on GUI
+
+
+#test print ships to console
+print(ship1.name, ship1.length, sep=" ")
+
+
+
