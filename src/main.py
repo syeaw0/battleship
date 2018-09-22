1603862fab72b0ce75dc35e6a70f0a97f59725bd
syeaw0 <syeaw0@csu.fullerton.edu>
2018-09-22 10:40:34 -0700

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,36 @@
 ###########
 # IMPORTS #
 ###########
-from ship import *
+import player as player_module
+import cmd
+
+#########
+# CLASS #
+#########
+class Console(cmd.Cmd):
+
+    # ATTRIBUTES
+    player = player_module.Player()
+    ai = player_module.Player()
+
+    def do_new(self, args):
+        """new - Starts a new game and initializes the player and ai"""
+        print("...Starting a new game\n")
+        self.prompt = "<player_info>\n" + "> "
+
+    def do_exit(self, args):
+        """exit - Exits the program"""
+        raise SystemExit
+
+    # OVERRIDEN METHODS
+    def emptyline(self):
+        pass
 
 ########
 # MAIN #
 ########
-ships = ShipSet(True)
-for ship in ships.ships:
-	print(ship.name + ": " + str(ship.length))
+if __name__ == "__main__":
+
+    console = Console()
+    console.prompt = "> "
+    console.cmdloop("BATTLESHIP")
