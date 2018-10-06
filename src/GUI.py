aa28731f5329178a56bb60fe4bd2581df9cf09d7
Tony Do on laptop <tonydo15@csu.fullerton.edu>
2018-10-05 23:17:35 -0700

    Update src/GUI.py

--- src/GUI.py
+++ src/GUI.py
@@ -6,19 +6,19 @@ import pygame
 
 
 pygame.init() # initiates pygame
-gameDisplay = pygame.display.set_mode ((800,600))
-pygame.display.set_caption('Battleship')
+gameDisplay = pygame.display.set_mode ((800,600)) #board size 
+pygame.display.set_caption('Battleship') #game title
 
 
 clock = pygame.time.Clock() #a real time clock
 
-crashed = False
+crashed = False #declares an end variable for when the user exits the game
 
-while not crashed:
+while not crashed: #while the user doesn't exit out the game
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
+        if event.type == pygame.QUIT: #if user presses the exit window
+            crashed = True #sets crash to true and then ends the game
 
         print(event)
 
@@ -27,4 +27,4 @@ while not crashed:
     clock.tick(30) #frames per second game. Can increase to 60
 
 
-pygame.quit() #quits pygame
+pygame.quit() #quits pygame and closes the GUI
